c409f739e5b88ec47d862eaaccf928283e006539
Bohdan Dorosh <doroshbohdan1@gmail.com>
2020-05-17 16:14:46 +0300

    Delete old version of the app

---
+++ EmailInfo.cs
@@ -0,0 +1,13 @@
+namespace cd_parser
+{
+	public class EmailInfo
+	{
+		public string SendTo { get; set; }
+		public string CarModel { get; set; }
+		public double Price { get; set; }
+		public string Pickup { get; set; }
+		public string Destination { get; set; }
+		public string Phone { get; set; }
+		public string PricePerMile { get; set; }
+	}
+}
--- migration/cd-parser/ClientApp/src/app/models/email-info.ts
+++
@@ -1,9 +0,0 @@
-export interface EmailInfo {
-    SendTo: string;
-    CarModel: string;
-    Price: number;
-    Pickup: string;
-    Destination: string;
-    Phone: string;
-    PricePerMile: string;
-}
